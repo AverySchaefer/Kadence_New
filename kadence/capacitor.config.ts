9b29c0137be05bf27d827b31b8b6bee82c78d525
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-01-30 12:22:28 -0500

    kinda adding in capacitor stuff? idk have to test more

---
+++ kadence/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'kadence',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
