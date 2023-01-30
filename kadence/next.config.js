b4f3af92105e6990d9cebe63a3cf08a4e3042106
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-01-30 10:47:02 -0500

    testing \'next run static \'

--- kadence/next.config.js
+++ kadence/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		images: {
+			unoptimized: true
+		}
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
