3ca67a65cd7eff32adff60cfeca5bf4861bcab24
Raymond Xie <raymond.xie.rx@gmail.com>
2023-02-03 15:30:56 -0500

    Fix image optimization feature error

--- kadence/next.config.js
+++ kadence/next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	experimental: {
-		images: {
-			unoptimized: true
-		}
-	}
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
