05808368569a28abab6bc8a8420643592700d1e9
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-02-06 16:08:37 -0500

    getting mongo fully connected YEAHHHHH

--- kadence/middleware/database.js
+++ kadence/middleware/database.js
@@ -1,13 +1,14 @@
 import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
 
-const client = new MongoClient('mongodb+srv://root:root@kadenceinstance1.lsepn.mongodb.net/?retryWrites=true&w=majority', {
+const client = new MongoClient('mongodb+srv://root:qVjKct8wfAeCnwOq@kadenceinstance1.lsepn.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 async function database(req, res, next) {
-  if (!client.isConnected()) await client.connect();
+  await client.connect();
+  //if (!client.isConnected()) await client.connect();
   req.dbClient = client;
   req.db = client.db('kadenceDatabase');
   return next();
