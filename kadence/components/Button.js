22ff3edd056dc5c5c21da2998e76bd15c8a35bc7
cstreit19 <colstonstreit@gmail.com>
2023-02-07 10:28:05 -0500

    Added login and register frontends

---
+++ kadence/components/Button.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/Button.module.css';
+
+export default function Button(props) {
+  return (
+    <button className={styles.button} {...props}>
+      {props.children}
+    </button>
+  );
+}
