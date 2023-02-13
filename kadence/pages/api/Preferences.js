e5c2bbf1c2f4b47a0428a3be79e5bc601370006e
raymond-xie <raymond.xie.rx@gmail.com>
2023-02-13 15:23:57 -0500

    Fix linting errors

--- kadence/pages/api/Preferences.js
+++ kadence/pages/api/Preferences.js
@@ -6,29 +6,29 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-    let doc = await req.db.collection('Preferences').findOne()
-    //console.log(doc);
-    res.json(doc);
+  const doc = await req.db.collection('Preferences').findOne();
+  // console.log(doc);
+  res.json(doc);
 });
 
 handler.post(async (req, res) => {
-    const doc = {
-        allowExplicit: req.allowExplicit,
-        lyricalInstrumental: req.lyricalInstrumental,
-        lyricalLanguage: req.lyricalLanguage,
-        minSongLength: req.minSongLength,
-        maxSongLength: req.maxSongLength,
-        minPlaylistLength: req.minPlaylistLength,
-        maxPlaylistLength: req.maxPlaylistLength,
-        faveGenres: req.faveGenres,
-        faveArtists: req.faveArtists,
-        blacklistedArtists: req.blacklistedArtists,
-        blacklistedSongs: req.blacklistedSongs
-    }
+  const doc = {
+    allowExplicit: req.allowExplicit,
+    lyricalInstrumental: req.lyricalInstrumental,
+    lyricalLanguage: req.lyricalLanguage,
+    minSongLength: req.minSongLength,
+    maxSongLength: req.maxSongLength,
+    minPlaylistLength: req.minPlaylistLength,
+    maxPlaylistLength: req.maxPlaylistLength,
+    faveGenres: req.faveGenres,
+    faveArtists: req.faveArtists,
+    blacklistedArtists: req.blacklistedArtists,
+    blacklistedSongs: req.blacklistedSongs,
+  };
 
-    const result = await req.db.collection('Preferences').insertOne(doc);
-    console.log("A document with the ID: ${result.insertedID} has been added");
-    res.json(doc);
-})
+  const result = await req.db.collection('Preferences').insertOne(doc);
+  console.log(`A document with the ID: ${result.insertedID} has been added`);
+  res.json(doc);
+});
 
 export default handler;
--- kadence/pages/api/users.js
+++ kadence/pages/api/users.js
@@ -6,10 +6,9 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-
-    let doc = await req.db.collection('Users').findOne()
-    console.log(doc);
-    res.json(doc);
+  const doc = await req.db.collection('Users').findOne();
+  console.log(doc);
+  res.json(doc);
 });
 
 export default handler;
