b7d265717f5a1676e464cd10d3e50ab845af2b17
cstreit19 <colstonstreit@gmail.com>
2023-02-17 07:33:53 -0500

    Added email field to register page, as well as frontend verification that passwords match. Also added endpoing /api/users/insert

--- kadence/components/Textbox.js
+++ kadence/components/Textbox.js
@@ -1,12 +1,5 @@
 import styles from '@/styles/Textbox.module.css';
 
-export default function Textbox({ name, placeholder = '', password = false }) {
-  return (
-    <input
-      className={styles.textInput}
-      type={password ? 'password' : 'text'}
-      name={name}
-      placeholder={placeholder}
-    />
-  );
+export default function Textbox(props) {
+    return <input className={styles.textInput} {...props} />;
 }
