8da337c83c516650a7ad7d04f9da0a897f616969
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-02-18 14:43:21 -0500

    finishing login.test.js and adding error info to main README

--- kadence/test/login.test.js
+++ kadence/test/login.test.js
@@ -10,3 +10,56 @@ test('Successful Login', () => {
         expect(response.status).toBe(200);
     });
 });
+
+test('Account Does Not Exist', () => {
+    const username = "JaneDoe";
+    const password = "passw0rd";
+
+    fetch('https://localhost:3000/api/users/login?' + new URLSearchParams({
+        username: username,
+        enteredPW: password,
+    })).then((response) => {
+        //console.log(response.status);
+        expect(response.status).toBe(400);
+    });
+});
+
+
+test('No Username Sent', () => {
+    const username = "";
+    const password = "passw0rd";
+
+    fetch('https://localhost:3000/api/users/login?' + new URLSearchParams({
+        username: username,
+        enteredPW: password,
+    })).then((response) => {
+        //console.log(response.status);
+        expect(response.status).toBe(400);
+    });
+});
+
+test('No Password Sent', () => {
+    const username = "JohnDoe";
+    const password = "";
+
+    fetch('https://localhost:3000/api/users/login?' + new URLSearchParams({
+        username: username,
+        enteredPW: password,
+    })).then((response) => {
+        //console.log(response.status);
+        expect(response.status).toBe(400);
+    });
+});
+
+test('Incorrect Password', () => {
+    const username = "JohnDoe";
+    const password = "wrongPassword";
+
+    fetch('https://localhost:3000/api/users/login?' + new URLSearchParams({
+        username: username,
+        enteredPW: password,
+    })).then((response) => {
+        //console.log(response.status);
+        expect(response.status).toBe(401);
+    });
+});
