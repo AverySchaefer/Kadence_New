6d19887c29c2cedcd42bcf03514b5b88270674d5
Jackson Rosenberg <rosenbe7@purdue.edu>
2023-02-20 14:33:38 -0500

    Adding logout [a.k.a. remove JWT from browser]

--- kadence/pages/api/users/logout.js
+++ kadence/pages/api/users/logout.js
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import nextConnect from 'next-connect';
+import middleware from '../../../middleware/database';
 
-export default function Logout() {
-    const { push } = useRouter();
+const handler = nextConnect();
+handler.use(middleware);
 
-    useEffect(() => {
-        localStorage.removeItem('jwt');
-        localStorage.removeItem('username');
-        push('/login');
-    }, [push]);
+handler.get(async (req, res) => {
+    console.log('Logout successful');
+    res.status(200).send(
+        'Logout successful, return back to login page.'
+    );
+});
 
-    return <></>;
-}
+export default handler;
