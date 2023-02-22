07029ca90cd4d3e62081f02ebcabede66f70ea3a
cstreit19 <colstonstreit@gmail.com>
2023-02-22 14:30:58 -0500

    Fixed settings page NetworkAPI broken

--- kadence/pages/settings.js
+++ kadence/pages/settings.js
@@ -96,9 +96,12 @@ export default function Settings() {
         async function fetchData() {
             try {
                 // Get User Data first
-                const userData = await NetworkAPI.get('/api/users/getUsers', {
-                    username: localStorage.getItem('username'),
-                });
+                const { data: userData } = await NetworkAPI.get(
+                    '/api/users/getUsers',
+                    {
+                        username: localStorage.getItem('username'),
+                    }
+                );
                 setProfilePrivate(userData.private);
                 setWaitToSave(userData.waitToSave);
                 setIntervalShort(userData.intervalShort);
@@ -110,7 +113,7 @@ export default function Settings() {
                 setMusicPrefId(userData.musicPrefs);
 
                 // Get preference data second (using musicPrefs id)
-                const prefData = await NetworkAPI.get(
+                const { data: prefData } = await NetworkAPI.get(
                     '/api/preferences/getPreferences',
                     {
                         uid: userData.musicPrefs,
@@ -175,7 +178,7 @@ export default function Settings() {
         } catch (err) {
             Dialog.alert({
                 title: 'Error Occurred',
-                message: `${err.status} ${err}`,
+                message: `${err.status} ${err.message}`,
             });
         }
     }
@@ -214,10 +217,10 @@ export default function Settings() {
                 title: 'Success',
                 message: `Settings successfully saved.`,
             });
-        } catch ({ status, error }) {
+        } catch (err) {
             Dialog.alert({
                 title: 'Error Occurred',
-                message: `Error occurred while saving: ${status} ${error}`,
+                message: `Error occurred while saving: ${err.message}`,
             });
         }
     }
