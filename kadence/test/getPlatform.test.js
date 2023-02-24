66f049c6d053b777650bd42b6fa44ec7064de4e0
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-02-23 23:23:52 -0500

    call me a structural engineer the way I be testing these platforms

--- kadence/test/getPlatform.test.js
+++ kadence/test/getPlatform.test.js
@@ -1,9 +1,9 @@
 import { testApiHandler } from 'next-test-api-route-handler';
-import handler from '../pages/api/preferences/getPreferences'; // TODO: change this to import the desired handler!
+import handler from '../pages/api/music/getPlatform'; // TODO: change this to import the desired handler!
 import { initTestDB, teardownTestDB } from './testDB';
 import { ObjectId } from 'mongodb';
 
-describe('GET /api/preferences/getPreferences', () => {
+describe('GET /api/music/getPlatform', () => {
     const correctUID = new ObjectId('63efd818545984788a2b0242');
     const incorrectUID = new ObjectId('63efd818545984788a2b0247');
     const emptyUID = '';
@@ -13,20 +13,11 @@ describe('GET /api/preferences/getPreferences', () => {
     beforeAll(async () => {
         ({ mongoServer, client, db } = await initTestDB(handler));
         await db
-            .collection('Preferences')
+            .collection('Music')
             .insertOne({
                 _id: new ObjectId('63efd818545984788a2b0242'), 
-                allowExplicit: true,
-                lyricalInstrumental: 'Both',
-                lyricalLanguage: 'English',
-                minSongLength: 1,
-                maxSongLength: 20,
-                minPlaylistLength: 1,
-                maxPlaylistLength: 100,
-                faveGenres: ['rock', 'alternative', 'metal'],
-                faveArtists: ['radiohead', 'tool'],
-                blacklistedArtists: ['oasis', 'genesis'],
-                blacklistedSongs: ['wonderwall'],
+                spotifyAccountID: '12345',
+                appleMusicAccountID: '67890',
             });
     });
 
