4d2c9629362aedfd97e994ff95029efeb808b263
raymond-xie <raymond.xie.rx@gmail.com>
2023-03-28 23:05:42 -0400

    Create interval mode pre-screen

--- kadence/pages/mode/interval.js
+++ kadence/pages/mode/interval.js
@@ -1,12 +1,84 @@
 import { useRouter } from 'next/router';
 import { PageLayout } from '@/components/';
-import { Button, Card } from '@mui/material';
+import { Button, Stack, Slider, Typography } from '@mui/material';
 import { useState, useEffect } from 'react';
 import NetworkAPI from '@/lib/networkAPI';
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/IntervalPage.module.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Box } from '@mui/system';
 
 export default function IntervalModeSetup() {
     const router = useRouter();
 
-    return <PageLayout title="Interval Mode" prevLink="/home"></PageLayout>;
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#69e267',
+            },
+        },
+    });
+    const [lowEnergyDuration, setLowEnergyDuration] = useState(40);
+    const [highEnergyDuration, setHighEnergyDuration] = useState(20);
+
+    return (
+        <PageLayout title="Interval Mode" prevLink="/home">
+            <ThemeProvider theme={theme}>
+                <Box sx={{ p: 2 }}>
+                    <h4>Select your interval lengths.</h4>
+                    <Box>
+                        <Typography id="high-energy-label">
+                            High Energy:
+                        </Typography>
+                        <Stack spacing={2} direction="row" sx={{ mb: 1 }}>
+                            <Slider
+                                min={5}
+                                step={1}
+                                max={60}
+                                value={highEnergyDuration}
+                                onChange={(e) => {
+                                    setHighEnergyDuration(
+                                        parseInt(e.target.value, 10)
+                                    );
+                                }}
+                                aria-labelledby="high-energy-label"
+                            />
+                            <p className={styles.muiSliderLabel}>
+                                {highEnergyDuration} minutes
+                            </p>
+                        </Stack>
+                    </Box>
+                    <Box>
+                        <Typography id="low-energy-label">
+                            Low Energy:
+                        </Typography>
+                        <Stack spacing={2} direction="row" sx={{ mb: 1 }}>
+                            <Slider
+                                min={5}
+                                step={1}
+                                max={60}
+                                value={lowEnergyDuration}
+                                onChange={(e) => {
+                                    setLowEnergyDuration(
+                                        parseInt(e.target.value, 10)
+                                    );
+                                }}
+                                aria-labelledby="low-energy-label"
+                            />
+                            <p className={styles.muiSliderLabel}>
+                                {lowEnergyDuration} minutes
+                            </p>
+                        </Stack>
+                    </Box>
+                    <Button
+                        variant="contained"
+                        sx={{ borderRadius: 3, width: '100%' }}
+                        className={`${styles.generateButton}`}
+                        onClick={() => {}}
+                    >
+                        Generate Playlist
+                    </Button>
+                </Box>
+            </ThemeProvider>
+        </PageLayout>
+    );
 }
