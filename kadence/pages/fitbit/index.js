52046656d994eb5d0c5af3c2295249dffc26361a
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-03-30 13:29:17 -0400

    fixing /fitbit/index.js

--- kadence/pages/fitbit/index.js
+++ kadence/pages/fitbit/index.js
@@ -8,7 +8,7 @@ import PageLayout from '@/components/PageLayout';
 import { useState } from 'react';
 import { Inter } from '@next/font/google';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Dialog } from '@mui/material';
+import { Dialog } from '@capacitor/dialog';
 
 const theme = createTheme({
     palette: {
@@ -33,9 +33,11 @@ export default function Display() {
             await NetworkAPI.patch('/api/users/update', userData);*/
             const redirectUri =
                 process.env.NODE_ENV === 'development'
-                    ? 'https://localhost:3000/fitbit'
+                    ? 'http://localhost:3000/fitbit'
                     : 'http://kadenceapp.com/fitbit';
-            process.window.location.assign(
+
+            console.log(redirectUri);
+            window.location.assign(
                 `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23QTD8&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight&code_challenge=vaC5salqWAhM5k50MMvXGPxkTQGyQeLa0NpP_K3689Y&code_challenge_method=S256&state=3j3k386j3x606u7000324b4x4n0b0o06&redirect_uri=${encodeURI(
                     redirectUri
                 )}`
