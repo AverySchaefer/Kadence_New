9276656663d8b1c2161fe3c97dfb7ab769fd3e26
raymond-xie <raymond.xie.rx@gmail.com>
2023-03-30 20:33:10 -0400

    Add files from workaround

---
+++ kadence/app.config.js
@@ -0,0 +1,5 @@
+const appConfig = {
+    apiHost: 'http://kadenceapp.com', // Set the domain where your API is hosted on
+};
+
+export default appConfig;
---
+++ kadence/lib/authHelper.js
@@ -0,0 +1,66 @@
+import appConfig from '../app.config';
+
+export default async function loginWithGithub(callback) {
+    /**
+     * Fetches the CSRF token, needed for all requests
+     * Bear in mind we need to set credentials to include
+     * https://developer.mozilla.org/en-US/docs/Web/API/Request/credentials
+     */
+    const { csrfToken } = await fetch(`${appConfig.apiHost}/api/auth/csrf`, {
+        credentials: 'include',
+    }).then((res) => res.json());
+
+    /**
+     * Generate an oAuth URL for the Twitter provider
+     */
+    const { url } = await fetch(`${appConfig.apiHost}/api/auth/signin/github`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            csrfToken,
+            json: 'true',
+            callbackUrl: `${appConfig.apiHost}`,
+        }),
+        redirect: 'follow',
+        credentials: 'include',
+    }).then((res) => res.json());
+
+    /**
+     * We are awaiting the import of the In App Browser as simply importing or requring it throws an error
+     */
+    const { InAppBrowser } = await import(
+        '@awesome-cordova-plugins/in-app-browser'
+    );
+
+    /**
+     * We open the instance of InAppBrowser with the URL and options
+     * https://cordova.apache.org/docs/en/11.x/reference/cordova-plugin-inappbrowser/
+     */
+    const ref = InAppBrowser.create(
+        url,
+        '_blank',
+        'location=yes,hidenavigationbuttons=yes,hideurlbar=yes,toolbarcolor=#f1f5f9'
+    );
+
+    if (ref) {
+        /**
+         * Here we can check if we've been successfully redirected, if we have we close the browser
+         * We did not specify a callbackUrl with /redirect though, so the browser won't close
+         */
+        ref.on('loadstart').subscribe((e) => {
+            const includesUrl = e.url.includes(`/redirect`);
+            if (includesUrl) {
+                ref.close();
+            }
+        });
+
+        /**
+         * We can also trigger a callback when we exit the InAppBrowser
+         */
+        ref.on('exit').subscribe(async () => {
+            callback();
+        });
+    }
+}
