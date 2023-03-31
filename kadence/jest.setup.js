eb6698ab0a367747921b2c40339eecb79304c3be
raymond-xie <raymond.xie.rx@gmail.com>
2023-03-31 00:32:56 -0400

    Mock router and local storage for front end tests

--- kadence/jest.setup.js
+++ kadence/jest.setup.js
@@ -2,4 +2,33 @@ import { setConfig } from 'next/config';
 import config from './next.config';
 import '@testing-library/jest-dom';
 
+// Mock next's router in tests
+// eslint-disable-next-line global-require
+jest.mock('next/router', () => require('next-router-mock'));
+
+class LocalStorageMock {
+    constructor() {
+        this.store = {};
+    }
+
+    clear() {
+        this.store = {};
+    }
+
+    getItem(key) {
+        return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+        this.store[key] = String(value);
+    }
+
+    removeItem(key) {
+        delete this.store[key];
+    }
+}
+
+// Replace the localStorage object with our mock
+global.localStorage = new LocalStorageMock();
+
 setConfig(config);
