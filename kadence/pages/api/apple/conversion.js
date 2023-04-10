873615be4100ff4817bce00e1997b6376f861ac2
Jackson Rosenberg <rosenbe7@purdue.edu>
2023-04-10 18:16:05 -0400

    Finishing Apple Music Search function (needs testing)

--- kadence/pages/api/apple/conversion.js
+++ kadence/pages/api/apple/conversion.js
@@ -51,7 +51,7 @@ async function saveApplePlaylist(playlistObjects, playlistName) {
                     console.log(status);
                     console.log(response2.error);
                     console.log(
-                        'There was an error creating the playlis with the songs'
+                        'There was an error creating the playlist with the songs'
                     );
                 }
             });
@@ -63,10 +63,22 @@ async function saveApplePlaylist(playlistObjects, playlistName) {
 
 /* TO DO */
 function appleSearch(songName, songArtists) {
-    const searchParameter = songArtists[0] + songName;
-    // Remove this when the function is finished:
-    // eslint-disable-next-line no-unused-vars
-    const APPLE_SEARCH_SONG_ENDPOINT = `https://api.music.apple.com/v1/catalog/us/search?term=${searchParameter}&limit=25&types=songs`;
+    let searchParameter = songArtists[0].concat('+') + songName;
+    searchParameter = searchParameter.place(' ', '+');
+
+    const APPLE_SEARCH_SONG_ENDPOINT = `https://api.music.apple.com/v1/catalog/us/search?term=${searchParameter}&limit=1&types=songs`;
+    fetch(APPLE_SEARCH_SONG_ENDPOINT, {
+        headers: getAppleHeader(),
+        method: 'GET',
+    })
+        .then((response) => {
+            const res = response.json();
+            const songID = res.results.songs.data.id;
+            return (songID, "songs"); // returns a tuple of (id: String, type: String)
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 async function extractSongInformation(token, songURI) {
