4f9596a5296e2000d09e7d0739462f3737967f4d
raymond-xie <raymond.xie.rx@gmail.com>
2023-04-12 15:24:29 -0400

    Polyfill TextEncoder/Decoder for tests

--- kadence/jest.setup.js
+++ kadence/jest.setup.js
@@ -1,6 +1,12 @@
+import { TextEncoder, TextDecoder } from 'util';
 import { setConfig } from 'next/config';
 import config from './next.config';
 import '@testing-library/jest-dom';
+
+// Polyfill TextEncoder/TextDecoder because the jest-dom environment doesn't have it
+// https://github.com/inrupt/solid-client-authn-js/issues/1676
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 // Mock next's router in tests
 // eslint-disable-next-line global-require
