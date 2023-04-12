812f5881ad89a48d636c2175dc9c425df088190b
raymond-xie <raymond.xie.rx@gmail.com>
2023-04-12 15:59:36 -0400

    Avery PLEASE use camelcase

--- kadence/test/api/getDevice.test.js
+++ kadence/test/api/getDevice.test.js
@@ -1,26 +1,24 @@
 import { testApiHandler } from 'next-test-api-route-handler';
-import handler from '../pages/api/devices/getDevices'; // TODO: change this to import the desired handler!
-import { initTestDB, teardownTestDB } from './testDB';
+import handler from '@/pages/api/devices/getDevices';
+import { initTestDB, teardownTestDB } from '@/test/testDB';
 import { ObjectId } from 'mongodb';
 
 describe('GET /api/devices/getDevices', () => {
-    const correct_id = new ObjectId('63efd818545984788a2b0242');
-    const incorrect_id = new ObjectId('63efd818545984788a2b0247');
-    const empty_id = '';
+    const correctId = new ObjectId('63efd818545984788a2b0242');
+    const incorrectId = new ObjectId('63efd818545984788a2b0247');
+    const emptyId = '';
     let mongoServer;
     let client;
     let db;
     beforeAll(async () => {
         ({ mongoServer, client, db } = await initTestDB(handler));
-        await db
-            .collection('Devices')
-            .insertOne({
-                _id: new ObjectId('63efd818545984788a2b0242'), 
-                deviceList: [],
-                selectedDeviceName: 'my watch',
-                selectedDeviceID: '12345',
-                tracking: true,
-            });
+        await db.collection('Devices').insertOne({
+            _id: new ObjectId('63efd818545984788a2b0242'),
+            deviceList: [],
+            selectedDeviceName: 'my watch',
+            selectedDeviceID: '12345',
+            tracking: true,
+        });
     });
 
     afterAll(async () => {
@@ -30,14 +28,13 @@ describe('GET /api/devices/getDevices', () => {
     it('should respond with 200 status code if doc is retrieved', async () => {
         await testApiHandler({
             handler,
-            url: `/users/getUsers?_id=${correct_id}`, // TODO: change this to the route path!
+            url: `/users/getUsers?_id=${correctId}`,
             test: async ({ fetch }) => {
                 const res = await fetch({
                     method: 'GET',
                 });
-                //console.log(res.status);
+
                 expect(res.status).toStrictEqual(200);
-                //await expect(res.json()).resolves.toStrictEqual({}); // TODO: change this object to the expected response!
             },
         });
     });
@@ -45,13 +42,12 @@ describe('GET /api/devices/getDevices', () => {
     it('should respond with 400 status code if doc is not in database', async () => {
         await testApiHandler({
             handler,
-            url: `/users/getUsers?_id=${incorrect_id}`, // TODO: change this to the route path!
+            url: `/users/getUsers?_id=${incorrectId}`,
             test: async ({ fetch }) => {
                 const res = await fetch({
                     method: 'GET',
                 });
                 expect(res.status).toStrictEqual(400);
-                //await expect(res.json()).resolves.toStrictEqual({}); // TODO: change this object to the expected response!
             },
         });
     });
@@ -59,13 +55,12 @@ describe('GET /api/devices/getDevices', () => {
     it('should respond with 400 status code if no _id is sent', async () => {
         await testApiHandler({
             handler,
-            url: `/users/getUsers?_id=${empty_id}`, // TODO: change this to the route path!
+            url: `/users/getUsers?_id=${emptyId}`,
             test: async ({ fetch }) => {
                 const res = await fetch({
                     method: 'GET',
                 });
                 expect(res.status).toStrictEqual(400);
-                //await expect(res.json()).resolves.toStrictEqual({}); // TODO: change this object to the expected response!
             },
         });
     });
--- kadence/test/api/getPref.test.js
+++ kadence/test/api/getPref.test.js
@@ -1,33 +1,31 @@
 import { testApiHandler } from 'next-test-api-route-handler';
-import handler from '../pages/api/preferences/getPreferences'; // TODO: change this to import the desired handler!
-import { initTestDB, teardownTestDB } from './testDB';
+import handler from '@/pages/api/preferences/getPreferences';
+import { initTestDB, teardownTestDB } from '@/test/testDB';
 import { ObjectId } from 'mongodb';
 
 describe('GET /api/preferences/getPreferences', () => {
-    const correct_id = new ObjectId('63efd818545984788a2b0242');
-    const incorrect_id = new ObjectId('63efd818545984788a2b0247');
-    const empty_id = '';
+    const correctId = new ObjectId('63efd818545984788a2b0242');
+    const incorrectId = new ObjectId('63efd818545984788a2b0247');
+    const emptyId = '';
     let mongoServer;
     let client;
     let db;
     beforeAll(async () => {
         ({ mongoServer, client, db } = await initTestDB(handler));
-        await db
-            .collection('Preferences')
-            .insertOne({
-                _id: new ObjectId('63efd818545984788a2b0242'), 
-                allowExplicit: true,
-                lyricalInstrumental: 'Both',
-                lyricalLanguage: 'English',
-                minSongLength: 1,
-                maxSongLength: 20,
-                minPlaylistLength: 1,
-                maxPlaylistLength: 100,
-                faveGenres: ['rock', 'alternative', 'metal'],
-                faveArtists: ['radiohead', 'tool'],
-                blacklistedArtists: ['oasis', 'genesis'],
-                blacklistedSongs: ['wonderwall'],
-            });
+        await db.collection('Preferences').insertOne({
+            _id: new ObjectId('63efd818545984788a2b0242'),
+            allowExplicit: true,
+            lyricalInstrumental: 'Both',
+            lyricalLanguage: 'English',
+            minSongLength: 1,
+            maxSongLength: 20,
+            minPlaylistLength: 1,
+            maxPlaylistLength: 100,
+            faveGenres: ['rock', 'alternative', 'metal'],
+            faveArtists: ['radiohead', 'tool'],
+            blacklistedArtists: ['oasis', 'genesis'],
+            blacklistedSongs: ['wonderwall'],
+        });
     });
 
     afterAll(async () => {
@@ -37,14 +35,13 @@ describe('GET /api/preferences/getPreferences', () => {
     it('should respond with 200 status code if doc is retrieved', async () => {
         await testApiHandler({
             handler,
-            url: `/users/getUsers?_id=${correct_id}`, // TODO: change this to the route path!
+            url: `/users/getUsers?_id=${correctId}`,
             test: async ({ fetch }) => {
                 const res = await fetch({
                     method: 'GET',
                 });
-                //console.log(res.status);
+
                 expect(res.status).toStrictEqual(200);
-                //await expect(res.json()).resolves.toStrictEqual({}); // TODO: change this object to the expected response!
             },
         });
     });
@@ -52,13 +49,12 @@ describe('GET /api/preferences/getPreferences', () => {
     it('should respond with 400 status code if doc is not in database', async () => {
         await testApiHandler({
             handler,
-            url: `/users/getUsers?_id=${incorrect_id}`, // TODO: change this to the route path!
+            url: `/users/getUsers?_id=${incorrectId}`,
             test: async ({ fetch }) => {
                 const res = await fetch({
                     method: 'GET',
                 });
                 expect(res.status).toStrictEqual(400);
-                //await expect(res.json()).resolves.toStrictEqual({}); // TODO: change this object to the expected response!
             },
         });
     });
@@ -66,13 +62,12 @@ describe('GET /api/preferences/getPreferences', () => {
     it('should respond with 400 status code if no _id is sent', async () => {
         await testApiHandler({
             handler,
-            url: `/users/getUsers?_id=${empty_id}`, // TODO: change this to the route path!
+            url: `/users/getUsers?_id=${emptyId}`,
             test: async ({ fetch }) => {
                 const res = await fetch({
                     method: 'GET',
                 });
                 expect(res.status).toStrictEqual(400);
-                //await expect(res.json()).resolves.toStrictEqual({}); // TODO: change this object to the expected response!
             },
         });
     });
