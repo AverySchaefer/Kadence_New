7ac4b43b98619465670c99cd646da5553c8b9bff
cstreit19 <colstonstreit@gmail.com>
2023-04-20 13:12:07 -0400

    Added seed tracks and genres where relevant to recommendations

--- kadence/pages/api/generation/mood.js
+++ kadence/pages/api/generation/mood.js
@@ -16,14 +16,11 @@ function generateSearchParams(prefData, chosenMood) {
     const maxSongLength = prefData.maxSongLength * 1000; // convert to ms
     const lyricalInstrumental = prefData.lyricalInstrumental / 100; // convert to 0-1 scale
 
-    // TODO: remove this and use genres or seed tracks instead
-    const songSeedID = '0c6xIDDpzE81m2q797ordA';
-
     if (chosenMood === 'happy') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
-            seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
+            seed_tracks: '4o6BgsqLIBViaGVbx5rbRk',
+            seed_genres: `${['happy', ...prefData.faveGenres].join(',')}`,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
             max_duration_ms: maxSongLength,
@@ -36,8 +33,8 @@ function generateSearchParams(prefData, chosenMood) {
     if (chosenMood === 'sad') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
-            seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
+            seed_tracks: '0Wf8Va6aRGqjQM5Wo2T1oI',
+            seed_genres: `${['sad', ...prefData.faveGenres].join(',')}`,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
             max_duration_ms: maxSongLength,
@@ -49,7 +46,7 @@ function generateSearchParams(prefData, chosenMood) {
     if (chosenMood === 'angry') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
+            seed_tracks: '5zUQZjVB6bfewBXWqsP9PY',
             seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
@@ -62,8 +59,8 @@ function generateSearchParams(prefData, chosenMood) {
     if (chosenMood === 'relaxed') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
-            seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
+            seed_tracks: '5ydoqOVn8eAZE56UXgYOAG',
+            seed_genres: `${['chill', ...prefData.faveGenres].join(',')}`,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
             max_duration_ms: maxSongLength,
@@ -77,7 +74,7 @@ function generateSearchParams(prefData, chosenMood) {
     if (chosenMood === 'energetic') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
+            seed_tracks: '7BY005dacJkbO6EPiOh2wb',
             seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
@@ -92,8 +89,8 @@ function generateSearchParams(prefData, chosenMood) {
     if (chosenMood === 'romantic') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
-            seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
+            seed_tracks: '1fujSajijBpJlr5mRGKHJN',
+            seed_genres: `${['romance', ...prefData.faveGenres].join(',')}`,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
             max_duration_ms: maxSongLength,
@@ -104,8 +101,8 @@ function generateSearchParams(prefData, chosenMood) {
     if (chosenMood === 'melancholy') {
         return new URLSearchParams({
             limit: 100,
-            seed_tracks: songSeedID,
-            seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
+            seed_tracks: '07XaOyTS5hyaWiUK1Bc3bR',
+            seed_genres: `${['rainy-day', ...prefData.faveGenres].join(',')}`,
             target_instrumentalness: lyricalInstrumental,
             min_duration_ms: minSongLength,
             max_duration_ms: maxSongLength,
@@ -118,7 +115,7 @@ function generateSearchParams(prefData, chosenMood) {
     }
     return new URLSearchParams({
         limit: 100,
-        seed_tracks: songSeedID,
+        seed_tracks: '4cOdK2wGLETKBW3PvgPWqT',
         seed_genres: `${prefData.faveGenres.join(',')}` || undefined,
         target_instrumentalness: lyricalInstrumental,
         min_duration_ms: minSongLength,
