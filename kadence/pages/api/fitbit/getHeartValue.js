4742769b764c038a224e3b5db553e234a1103c82
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2023-04-21 18:35:13 -0400

    starting to fix getHeartValue url generation

--- kadence/pages/api/fitbit/getHeartValue.js
+++ kadence/pages/api/fitbit/getHeartValue.js
@@ -3,9 +3,13 @@ import nextConnect from 'next-connect';
 import middleware from '../../../middleware/database';
 
 const GET_VALUE_BASE_URL =
-    'https://api.fitbit.com/1/user/-/activities/heart/date/today/today/1sec/time/';
+    'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec/time/';
 
 const handler = nextConnect();
+var currHourString = '';
+var currMinuteString = '';
+var pastHourString = '';
+var pastMinuteString = '';
 
 handler.use(middleware);
 
@@ -21,15 +25,37 @@ async function createURL() {
         pastMinute = 59;
         pastHour -= 1;
     }
-    //TODO Add in handling for if hour or minute are only 1 digit (need to add leading zero so its HH:MM)
+   
+    //Make sure that times are in HH:MM format, even if hours or minutes are less than 10
+    //TODO This is still getting a "Failed to parse URL from [object Promise]" error
+    if (currHour < 10) {
+        currHourString = '0' + currHour.toString();
+    } else {
+        currHourString = currHour.toString();
+    }
+    if (currMinute < 10) {
+        currMinuteString = '0' + currMinute.toString();
+    } else {
+        currMinuteString = currMinute.toString();
+    }
+    if (pastHour < 10) {
+        pastHourString = '0' + pastHour.toString();
+    } else {
+        pastHourString = pastHour.toString();
+    }
+    if (pastMinute < 10) {
+        pastMinuteString = '0' + pastMinute.toString();
+    } else {
+        pastMinuteString = pastMinute.toString();
+    }
 
     return `${
-        GET_VALUE_BASE_URL + pastHour
-    }:${pastMinute}/${currHour}:${currMinute}.json`;
+        GET_VALUE_BASE_URL + pastHourString
+    }:${pastMinuteString}/${currHourString}:${currMinuteString}.json`;
 }
 
 async function getValue(token) {
-    const GET_VALUE_URL = 'https://api.fitbit.com/1/user/-/activities/heart/date/today/today/1sec/time/12:02/12:03.json'//createURL();
+    const GET_VALUE_URL = createURL();
     console.log(GET_VALUE_URL);
     return fetch(GET_VALUE_URL, {
         headers: {
