8e6899f432cc1330a8b3edd716dd5d6d39340a63
Raymond Xie <raymond.xie.rx@gmail.com>
2023-04-27 16:57:47 -0400

    Remove local storage mock

--- kadence/jest.setup.js
+++ kadence/jest.setup.js
@@ -12,29 +12,4 @@ global.TextDecoder = TextDecoder;
 // eslint-disable-next-line global-require
 jest.mock('next/router', () => require('next-router-mock'));
 
-class LocalStorageMock {
-    constructor() {
-        this.store = {};
-    }
-
-    clear() {
-        this.store = {};
-    }
-
-    getItem(key) {
-        return this.store[key] || null;
-    }
-
-    setItem(key, value) {
-        this.store[key] = String(value);
-    }
-
-    removeItem(key) {
-        delete this.store[key];
-    }
-}
-
-// Replace the localStorage object with our mock
-global.localStorage = new LocalStorageMock();
-
 setConfig(config);
