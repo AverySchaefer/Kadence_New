b7c0606fdb2e60944a85d54ccf418ca4e4d27094
Jackson Rosenberg <rosenbe7@purdue.edu>
2023-04-27 15:08:23 -0400

    Adding Colston's changes to gen files

--- kadence/lib/generation/getMoodRecommendationsSpotifyOnly.js
+++ kadence/lib/generation/getMoodRecommendationsSpotifyOnly.js
@@ -1,35 +1,39 @@
 import generateMoodParamsSpotifyOnly from '@/lib/generation/generateMoodParamsSpotifyOnly';
 import getCurrentSong from '@/lib/generation/getCurrentSong';
 import refreshToken from '@/lib/spotify/refreshToken';
+import getMoodRecommendations from '@/lib/generation/getMoodRecommendations';
 
 export default async function getMoodRecommendationsSpotifyOnly(token, chosenMood, prefData) {
     const { access_token: accessToken } = await refreshToken(token);
-    const response = await getCurrentSong(token);
-    const songItem = await response.json();
-    const songSeedID = songItem.item.id;
-    console.log(songSeedID);
-    const searchParameters = await generateMoodParamsSpotifyOnly(
-        prefData,
-        songSeedID,
-        chosenMood
-    );
-
-    const RECOMMENDATIONS_ENDPOINT = `https://api.spotify.com/v1/recommendations?`;
     try {
-        const recs = await fetch(
-            RECOMMENDATIONS_ENDPOINT + searchParameters,
-            {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            }
+        const response = await getCurrentSong(token);
+        const songItem = await response.json();
+        const songSeedID = songItem.item.id;
+        const searchParameters = await generateMoodParamsSpotifyOnly(
+            prefData,
+            songSeedID,
+            chosenMood
         );
-        const results = await recs.json();
 
-        return results.tracks;
+        const RECOMMENDATIONS_ENDPOINT = `https://api.spotify.com/v1/recommendations?`;
+        try {
+            const recs = await fetch(
+                RECOMMENDATIONS_ENDPOINT + searchParameters,
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                }
+            );
+            const results = await recs.json();
+
+            return results.tracks;
+        } catch (err) {
+            console.log('Something went wrong fetching recs from Spotify');
+            console.log(err);
+            return null;
+        }
     } catch (err) {
-        console.log('Something went wrong fetching recs from Spotify');
-        console.log(err);
-        return null;
+        return getMoodRecommendations(prefData, chosenMood);
     }
 }
